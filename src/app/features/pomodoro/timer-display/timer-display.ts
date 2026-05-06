f8e0f6ff1a1dcf56f5968c2e08a58abede19056c
Sara Morales Caravaca <56777997+milliways-png@users.noreply.github.com>
2026-05-06 11:53:14 -0600

    Feature start pause button and restart button
    * functioning start/pause button
    
    * change labels and add restart button

--- src/app/features/pomodoro/timer-display/timer-display.ts
+++ src/app/features/pomodoro/timer-display/timer-display.ts
@@ -13,23 +13,51 @@ import { AsyncPipe } from '@angular/common';
 })
 export class TimerDisplay {
   timeLeft: number = 0;
-  running: boolean = false;
+  isRunning: boolean = false;
 
   initialSeconds: number = 120; // 120 secs = 2 min
   private countdownSource = new BehaviorSubject<number>(this.initialSeconds);
   countdown$ = this.countdownSource.asObservable();
 
   private timerSub?: Subscription;
-  
+ 
+  toggleTimer() {
+    if (this.isRunning) {
+      this.pause();
+    } else {
+      this.start();
+    }
+  }
+
+  pause() {
+    if (this.timerSub) {
+      this.timerSub.unsubscribe(); // halt flow
+      this.isRunning = false;
+    }    
+  }
+
   start() {
     // avoid multiple subscriptions
+    if (this.isRunning){return}; // avoid duplicate instances
+
+    this.isRunning = true;
     if (this.timerSub) this.timerSub.unsubscribe();
 
+    const currentSeconds = this.countdownSource.value;
     this.timerSub = timer(0, 1000).pipe(
-      take(this.initialSeconds + 1),
-      map(tick => this.initialSeconds - tick)
-    ).subscribe(val => this.countdownSource.next(val));
+      take(currentSeconds + 1),
+      map(tick => currentSeconds - tick)
+    ).subscribe({
+      next: (val) => this.countdownSource.next(val),
+      complete: () => (this.isRunning = false)
+    });
   }
+
+  reset() {
+    this.pause();
+    this.countdownSource.next(this.initialSeconds);
+  }
+
   ngOnDestroy() {
     this.timerSub?.unsubscribe();
   }
